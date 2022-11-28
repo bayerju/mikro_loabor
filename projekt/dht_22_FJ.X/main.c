ea8265f46fbd42ce5ef97c612b524c9814f82634
bayerju <bayerju@users.noreply.github.com>
2022-11-28 16:09:10 +0100

    init project

---
+++ projekt/dht_22_FJ.X/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: Julian
+ *
+ * Created on 28. November 2022, 16:07
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
