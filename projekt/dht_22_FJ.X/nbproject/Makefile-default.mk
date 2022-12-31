4b25fa1cd664c808d13375fe42729f9a521e83ba
bayerju <bayerju@users.noreply.github.com>
2022-12-31 01:35:02 +0100

    test readded uart

--- projekt/dht_22_FJ.X/nbproject/Makefile-default.mk
+++ projekt/dht_22_FJ.X/nbproject/Makefile-default.mk
@@ -57,17 +57,17 @@ OBJECTDIR=build/${CND_CONF}/${IMAGE_TYPE}
 DISTDIR=dist/${CND_CONF}/${IMAGE_TYPE}
 
 # Source Files Quoted if spaced
-SOURCEFILES_QUOTED_IF_SPACED=main.c timer.c dht.c pll.c timerstart.c global_definitions.c PIC_init.c OLED.c i2c_routines.c uart.c uart_dsPIC.c
+SOURCEFILES_QUOTED_IF_SPACED=main.c timer.c dht.c pll.c timerstart.c global_definitions.c PIC_init.c OLED.c i2c_routines.c uart_dsPIC.c uart.c
 
 # Object Files Quoted if spaced
-OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/main.o ${OBJECTDIR}/timer.o ${OBJECTDIR}/dht.o ${OBJECTDIR}/pll.o ${OBJECTDIR}/timerstart.o ${OBJECTDIR}/global_definitions.o ${OBJECTDIR}/PIC_init.o ${OBJECTDIR}/OLED.o ${OBJECTDIR}/i2c_routines.o ${OBJECTDIR}/uart.o ${OBJECTDIR}/uart_dsPIC.o
-POSSIBLE_DEPFILES=${OBJECTDIR}/main.o.d ${OBJECTDIR}/timer.o.d ${OBJECTDIR}/dht.o.d ${OBJECTDIR}/pll.o.d ${OBJECTDIR}/timerstart.o.d ${OBJECTDIR}/global_definitions.o.d ${OBJECTDIR}/PIC_init.o.d ${OBJECTDIR}/OLED.o.d ${OBJECTDIR}/i2c_routines.o.d ${OBJECTDIR}/uart.o.d ${OBJECTDIR}/uart_dsPIC.o.d
+OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/main.o ${OBJECTDIR}/timer.o ${OBJECTDIR}/dht.o ${OBJECTDIR}/pll.o ${OBJECTDIR}/timerstart.o ${OBJECTDIR}/global_definitions.o ${OBJECTDIR}/PIC_init.o ${OBJECTDIR}/OLED.o ${OBJECTDIR}/i2c_routines.o ${OBJECTDIR}/uart_dsPIC.o ${OBJECTDIR}/uart.o
+POSSIBLE_DEPFILES=${OBJECTDIR}/main.o.d ${OBJECTDIR}/timer.o.d ${OBJECTDIR}/dht.o.d ${OBJECTDIR}/pll.o.d ${OBJECTDIR}/timerstart.o.d ${OBJECTDIR}/global_definitions.o.d ${OBJECTDIR}/PIC_init.o.d ${OBJECTDIR}/OLED.o.d ${OBJECTDIR}/i2c_routines.o.d ${OBJECTDIR}/uart_dsPIC.o.d ${OBJECTDIR}/uart.o.d
 
 # Object Files
-OBJECTFILES=${OBJECTDIR}/main.o ${OBJECTDIR}/timer.o ${OBJECTDIR}/dht.o ${OBJECTDIR}/pll.o ${OBJECTDIR}/timerstart.o ${OBJECTDIR}/global_definitions.o ${OBJECTDIR}/PIC_init.o ${OBJECTDIR}/OLED.o ${OBJECTDIR}/i2c_routines.o ${OBJECTDIR}/uart.o ${OBJECTDIR}/uart_dsPIC.o
+OBJECTFILES=${OBJECTDIR}/main.o ${OBJECTDIR}/timer.o ${OBJECTDIR}/dht.o ${OBJECTDIR}/pll.o ${OBJECTDIR}/timerstart.o ${OBJECTDIR}/global_definitions.o ${OBJECTDIR}/PIC_init.o ${OBJECTDIR}/OLED.o ${OBJECTDIR}/i2c_routines.o ${OBJECTDIR}/uart_dsPIC.o ${OBJECTDIR}/uart.o
 
 # Source Files
-SOURCEFILES=main.c timer.c dht.c pll.c timerstart.c global_definitions.c PIC_init.c OLED.c i2c_routines.c uart.c uart_dsPIC.c
+SOURCEFILES=main.c timer.c dht.c pll.c timerstart.c global_definitions.c PIC_init.c OLED.c i2c_routines.c uart_dsPIC.c uart.c
 
 
 
@@ -149,17 +149,17 @@ ${OBJECTDIR}/i2c_routines.o: i2c_routines.c  .generated_files/flags/default/6db3
 	@${RM} ${OBJECTDIR}/i2c_routines.o 
 	${MP_CC} $(MP_EXTRA_CC_PRE)  i2c_routines.c  -o ${OBJECTDIR}/i2c_routines.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/i2c_routines.o.d"      -g -D__DEBUG -D__MPLAB_DEBUGGER_ICD4=1    -omf=elf -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off    -mdfp="${DFP_DIR}/xc16"
 	
-${OBJECTDIR}/uart.o: uart.c  .generated_files/flags/default/21a4659b29c7046842ea3dafda1423a0437fcca0 .generated_files/flags/default/74d464f3899599845d713a1d015fd0bf2695bd19
-	@${MKDIR} "${OBJECTDIR}" 
-	@${RM} ${OBJECTDIR}/uart.o.d 
-	@${RM} ${OBJECTDIR}/uart.o 
-	${MP_CC} $(MP_EXTRA_CC_PRE)  uart.c  -o ${OBJECTDIR}/uart.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/uart.o.d"      -g -D__DEBUG -D__MPLAB_DEBUGGER_ICD4=1    -omf=elf -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off    -mdfp="${DFP_DIR}/xc16"
-	
 ${OBJECTDIR}/uart_dsPIC.o: uart_dsPIC.c  .generated_files/flags/default/6f8c3fba3583dda228526d5059e452f601b4c50e .generated_files/flags/default/74d464f3899599845d713a1d015fd0bf2695bd19
 	@${MKDIR} "${OBJECTDIR}" 
 	@${RM} ${OBJECTDIR}/uart_dsPIC.o.d 
 	@${RM} ${OBJECTDIR}/uart_dsPIC.o 
 	${MP_CC} $(MP_EXTRA_CC_PRE)  uart_dsPIC.c  -o ${OBJECTDIR}/uart_dsPIC.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/uart_dsPIC.o.d"      -g -D__DEBUG -D__MPLAB_DEBUGGER_ICD4=1    -omf=elf -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off    -mdfp="${DFP_DIR}/xc16"
+	
+${OBJECTDIR}/uart.o: uart.c  .generated_files/flags/default/21a4659b29c7046842ea3dafda1423a0437fcca0 .generated_files/flags/default/74d464f3899599845d713a1d015fd0bf2695bd19
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/uart.o.d 
+	@${RM} ${OBJECTDIR}/uart.o 
+	${MP_CC} $(MP_EXTRA_CC_PRE)  uart.c  -o ${OBJECTDIR}/uart.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/uart.o.d"      -g -D__DEBUG -D__MPLAB_DEBUGGER_ICD4=1    -omf=elf -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off    -mdfp="${DFP_DIR}/xc16"
 	
 else
 ${OBJECTDIR}/main.o: main.c  .generated_files/flags/default/4b132a5091cd6af34a4b9deddfd5d799b725579e .generated_files/flags/default/74d464f3899599845d713a1d015fd0bf2695bd19
@@ -216,17 +216,17 @@ ${OBJECTDIR}/i2c_routines.o: i2c_routines.c  .generated_files/flags/default/7b6a
 	@${RM} ${OBJECTDIR}/i2c_routines.o 
 	${MP_CC} $(MP_EXTRA_CC_PRE)  i2c_routines.c  -o ${OBJECTDIR}/i2c_routines.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/i2c_routines.o.d"        -g -omf=elf -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off    -mdfp="${DFP_DIR}/xc16"
 	
-${OBJECTDIR}/uart.o: uart.c  .generated_files/flags/default/95db13e6087e6ed8f998e3326f10290951a97dc4 .generated_files/flags/default/74d464f3899599845d713a1d015fd0bf2695bd19
-	@${MKDIR} "${OBJECTDIR}" 
-	@${RM} ${OBJECTDIR}/uart.o.d 
-	@${RM} ${OBJECTDIR}/uart.o 
-	${MP_CC} $(MP_EXTRA_CC_PRE)  uart.c  -o ${OBJECTDIR}/uart.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/uart.o.d"        -g -omf=elf -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off    -mdfp="${DFP_DIR}/xc16"
-	
 ${OBJECTDIR}/uart_dsPIC.o: uart_dsPIC.c  .generated_files/flags/default/9a68d1c295c575dbd9a47fcf0b77c365b9eeef53 .generated_files/flags/default/74d464f3899599845d713a1d015fd0bf2695bd19
 	@${MKDIR} "${OBJECTDIR}" 
 	@${RM} ${OBJECTDIR}/uart_dsPIC.o.d 
 	@${RM} ${OBJECTDIR}/uart_dsPIC.o 
 	${MP_CC} $(MP_EXTRA_CC_PRE)  uart_dsPIC.c  -o ${OBJECTDIR}/uart_dsPIC.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/uart_dsPIC.o.d"        -g -omf=elf -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off    -mdfp="${DFP_DIR}/xc16"
+	
+${OBJECTDIR}/uart.o: uart.c  .generated_files/flags/default/95db13e6087e6ed8f998e3326f10290951a97dc4 .generated_files/flags/default/74d464f3899599845d713a1d015fd0bf2695bd19
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/uart.o.d 
+	@${RM} ${OBJECTDIR}/uart.o 
+	${MP_CC} $(MP_EXTRA_CC_PRE)  uart.c  -o ${OBJECTDIR}/uart.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/uart.o.d"        -g -omf=elf -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off    -mdfp="${DFP_DIR}/xc16"
 	
 endif
 
